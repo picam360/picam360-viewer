6d7327ed44271e723e2858f98c3506099cbfdf03
takumadx <hirayama.takuma@ohmydigifab.com>
2017-08-30 12:28:30 +0900

    peer.picam360.com

--- js/index.js
+++ js/index.js
@@ -25,6 +25,9 @@ var PT_CMD = 101;
 var PT_CAM_BASE = 110;
 var P2P_API_KEY = "v8df88o1y4zbmx6r";
 var PUBLIC_VIEWER = "http://picam360.github.io/picam360-viewer/";
+var SIGNALING_HOST = "peer.picam360.com";
+var SIGNALING_PORT = 443;
+var SIGNALING_SECURE = true;
 
 var app = (function() {
 	var tilt = 0;
@@ -533,6 +536,9 @@ var app = (function() {
 				execCopy(viewer_url);
 				alert("The p2p link was copied to clip board : " + viewer_url);
 				peer = new Peer(p2p_uuid, {
+					host : SIGNALING_HOST,
+					port : SIGNALING_PORT,
+					secure : SIGNALING_SECURE,
 					key : P2P_API_KEY,
 					debug : debug
 				});
@@ -569,6 +575,9 @@ var app = (function() {
 				});
 			} else {
 				peer = new Peer({
+					host : SIGNALING_HOST,
+					port : SIGNALING_PORT,
+					secure : SIGNALING_SECURE,
 					key : P2P_API_KEY,
 					debug : debug
 				});
