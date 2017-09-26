f861848854576214ad3e7d7562c8b41660dc6115
takumadx <hirayama.takuma@ohmydigifab.com>
2017-09-26 12:01:36 +0900

    default fov

--- js/index.js
+++ js/index.js
@@ -32,8 +32,8 @@ var SIGNALING_SECURE = true;
 var app = (function() {
 	var tilt = 0;
 	var socket;
-	var m_fov_margin = 10;
-	var m_view_fov = 120;
+	var m_fov_margin = 30;
+	var m_view_fov = 90;
 	var target_fps = 10;
 	var auto_scroll = false;
 	var view_offset_lock = false;
