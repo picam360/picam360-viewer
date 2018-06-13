484b5b3c4af405ac39b1311eee310b2131a74390
takumadx <hirayama.takuma@ohmydigifab.com>
2018-06-13 16:33:19 +0900

    map viewed

--- plugins/map/map.js
+++ plugins/map/map.js
@@ -1,6 +1,7 @@
 var create_plugin = (function() {
 	var m_plugin_host = null;
 	var m_is_init = false;
+	
 	function decodeUtf8(data) {
 		var result = "";
 		var i = 0;
@@ -38,12 +39,46 @@ var create_plugin = (function() {
 		}
 		return result;
 	}
+	function map_load() {
+		var options = {
+			controls : [new OpenLayers.Control.Navigation(),
+				new OpenLayers.Control.NavToolbar(),
+				new OpenLayers.Control.PanZoomBar(),
+				new OpenLayers.Control.ScaleLine(),
+				// new OpenLayers.Control.ZoomPanel(),
+				new OpenLayers.Control.Attribution()],
+		};
+
+		var map = new OpenLayers.Map("demoMap", options);
+		map.addLayer(new OpenLayers.Layer.OSM());
+
+		console.log(map.getProjectionObject().getCode());
+
+		map.setCenter(new OpenLayers.LonLat(139.76, 35.68)
+			.transform(new OpenLayers.Projection("EPSG:4326"), // WGS84
+			new OpenLayers.Projection("EPSG:3857") // Google Map / OSM / etc
+			// の球面メルカトル図法,
+			// http://wiki.openstreetmap.org/wiki/Projection
+			), 15);
+
+	}
 	function init(plugin) {
 		m_plugin_host.getFile("plugins/map/map.html", function(chunk_array) {
 			var txt = decodeUtf8(chunk_array[0]);
 			var node = $.parseHTML(txt);
 			$('body').append(node);
+			var map_page = document.getElementById("map_page");
 			ons.compile(node[0]);
+
+			app.navi.on('postpush', function(event) {
+				if (event.enterPage.name == "map.html") {
+					map_load();
+				}
+			});
+
+			var script = document.createElement('script');
+			script.src = "http://www.openlayers.org/api/OpenLayers.js";
+			document.head.appendChild(script);
 		});
 		m_plugin_host.getFile("plugins/map/map_list_item.html", function(
 			chunk_array) {
