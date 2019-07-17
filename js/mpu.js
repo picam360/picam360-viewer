789ccd76a4efe058f3ab32d0bb2083835b3c8e9d
takumadx <hirayama.takuma@ohmydigifab.com>
2019-07-17 12:39:07 +0900

    revert previouse

--- js/mpu.js
+++ js/mpu.js
@@ -76,12 +76,6 @@ function MPU(plugin_host) {
 		},
 
 		initDeviceOrientationEventLisener : function() {
-			window.ondeviceorientation = function(evt){
-				console.log(evt);
-			}
-			window.onorientationchange = function(evt){
-				console.log(evt);
-			}
 			window
 				.addEventListener('deviceorientation', function(attitude) {
 					if (attitude['detail']) {
