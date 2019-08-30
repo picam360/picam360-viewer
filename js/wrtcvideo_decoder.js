dac0e95eaa58ced7e6c11eb393c60623ba262abc
takumadx <hirayama.takuma@ohmydigifab.com>
2019-08-31 00:08:02 +0900

    ncc

--- js/wrtcvideo_decoder.js
+++ js/wrtcvideo_decoder.js
@@ -12,12 +12,32 @@ function WRTCVideoDecoder(callback) {
 	var m_videoImageContext = null;
 	var m_receiver = null;
 	
-	function uuid_dif(uuid1, uuid2){
+	function uuid_abs(uuid1, uuid2){
 		var sum = 0;
 		for(var i=0;i<uuid1.length;i++){
 			sum += Math.abs(uuid1[i]-uuid2[i]);
 		}
 		return sum;
+	}
+	function uuid_ncc(uuid1, uuid2){
+		var sum1 = 0;
+		var sum2 = 0;
+		var sum12 = 0;
+		for(var i=0;i<uuid1.length;i++){
+			sum1 += uuid1[i]*uuid1[i];
+			sum2 += uuid2[i]*uuid2[i];
+			sum12 += uuid1[i]*uuid2[i];
+		}
+		return sum12 / Math.sqrt(sum1*sum2);
+	}
+	
+	function get_uuid_from_video(video, ctx){
+		var uuid = new Uint8Array(16);
+		var apx = ctx.getImageData(0,0, 16, 1);
+		for(var i=0;i<16;i++){
+			uuid[i] = 0.299*apx.data[i*4+0]+0.587*apx.data[i*4+1]+0.114*apx.data[i*4+2];
+		}
+		return uuid;
 	}
 	
 	function GetQueryString() {
@@ -41,21 +61,22 @@ function WRTCVideoDecoder(callback) {
 
 	var m_is_init = false;
 	var m_first_frame = false;
+	var m_can_play = false;
 	var info = {};
 	var packet_pool = [];
 
 	var self = {
 		new_image_handler: function(imageBitmap, uuid) {
-			var min_i = m_decoded_frame_num;
-			var min_dif = Number.MAX_SAFE_INTEGER;
+			var max_i = m_decoded_frame_num;
+			var max_ncc = 0;
 			for(var i in m_frame_info){
-				var dif = uuid_dif(m_frame_info[i].uuid, uuid);
-				if(dif < min_dif){
-					min_dif = dif;
-					min_i = i;
+				var ncc = uuid_ncc(m_frame_info[i].uuid, uuid);
+				if(ncc > max_ncc){
+					max_ncc = ncc;
+					max_i = i;
 				}
 			}
-			m_decoded_frame_num = parseInt(min_i);
+			m_decoded_frame_num = parseInt(max_i);
 
 			if (m_frame_callback) {
 				var info = m_frame_info[m_decoded_frame_num];
@@ -79,50 +100,57 @@ function WRTCVideoDecoder(callback) {
 			m_frame_info = frame_info;
 		},
 		init: function() {
-			if (!m_is_init && m_first_frame && m_video) {
+			if (!m_is_init && m_first_frame && m_can_play) {
 				m_is_init = true;
 
-				function createButton(text, x, y, context, func) {
-					var button = document.createElement("input");
-					button.type = "button";
-					button.value = text;
-					button.style.position = 'absolute';
-					button.style.left = window.innerWidth * x + 'px';
-					button.style.top = window.innerHeight * y + 'px';
+				m_videoImage = document.createElement('canvas');
+				m_videoImage.width = m_video.videoWidth;
+				m_videoImage.height = m_video.videoHeight;
+				m_videoImageContext = m_videoImage.getContext('2d');
+				m_videoImageContext.fillStyle = '#000000';
+				m_videoImageContext.fillRect(0, 0, m_videoImage.width, m_videoImage.height);
 
-					button.onclick = func;
-					context.appendChild(button);
-				}
-				createButton('video start', 0.5, 0.5, document.body, (e) => {
-					m_video.play().catch((err) => {
-						console.log(err);
-					});
-
-					m_videoImage = document.createElement('canvas');
-					m_videoImage.width = m_video.videoWidth;
-					m_videoImage.height = m_video.videoHeight;
-					m_videoImageContext = m_videoImage.getContext('2d');
-					m_videoImageContext.fillStyle = '#000000';
-					m_videoImageContext.fillRect(0, 0, m_videoImage.width, m_videoImage.height);
-
-					var last_currentTime = m_video.currentTime;
+				function start_video_poling(){
+					var last_uuid = new Uint8Array(16);
 					setInterval(function() {
-						var currentTime = m_video.currentTime;
-						if (last_currentTime != currentTime) {
-							m_videoImageContext.drawImage(m_video, 0, 0, m_videoImage.width, m_videoImage.height);
-							var uuid = new Uint8Array(16);
-							var apx = m_videoImageContext.getImageData(0,0, 16, 1);
-							for(var i=0;i<16;i++){
-								uuid[i] = 0.299*apx.data[i*4+0]+0.587*apx.data[i*4+1]+0.114*apx.data[i*4+2];
-							}
-							window.createImageBitmap(m_videoImage).then(imageBitmap => {
-								self.new_image_handler(imageBitmap, uuid);
-							});
-							last_currentTime = currentTime;
+						m_video.currentTime += 33/1000;
+						m_videoImageContext.drawImage(m_video, 0, 0, m_videoImage.width, m_videoImage.height);
+						var uuid = get_uuid_from_video(m_video, m_videoImageContext);
+						if (uuid_abs(last_uuid, uuid) == 0) {
+							return;
 						}
-					}, 10);
-					document.body.removeChild(e.srcElement);
-				});
+						window.createImageBitmap(m_videoImage).then(imageBitmap => {
+							self.new_image_handler(imageBitmap, uuid);
+						});
+						last_uuid = uuid;
+					}, 33); // 30hz
+				}
+
+				var uuid_zero = new Uint8Array(16);
+				m_videoImageContext.drawImage(m_video, 0, 0, m_videoImage.width, m_videoImage.height);
+				var uuid = get_uuid_from_video(m_video, m_videoImageContext);
+				if (uuid_abs(uuid_zero, uuid) == 0) {//need to play
+					function createButton(text, x, y, context, func) {
+						var button = document.createElement("input");
+						button.type = "button";
+						button.value = text;
+						button.style.position = 'absolute';
+						button.style.left = window.innerWidth * x + 'px';
+						button.style.top = window.innerHeight * y + 'px';
+	
+						button.onclick = func;
+						context.appendChild(button);
+					}
+					createButton('video start', 0.5, 0.5, document.body, (e) => {
+						m_video.play().catch((err) => {
+							console.log(err);
+						});
+						start_video_poling();
+						document.body.removeChild(e.srcElement);
+					});
+				} else {
+					start_video_poling();
+				}
 			}
 		},
 		set_stream: function(obj, receiver) {
@@ -132,7 +160,12 @@ function WRTCVideoDecoder(callback) {
 			m_video.srcObject = obj;
 			m_video.load();
 
-			self.init();
+			m_video.addEventListener("canplay", function() {
+				console.log("video canplay!");
+				m_can_play = true;
+				self.init();
+				m_video.removeEventListener("canplay", arguments.callee, false);
+			});
 		},
 		set_frame_callback: function(callback) {
 			m_frame_callback = callback;
