b1660c9527631363411a17fe5a5f790c264c1d70
takumadx <hirayama.takuma@ohmydigifab.com>
2019-11-26 11:54:53 +0900

    try to fix image jaming

--- js/vpm_loader.js
+++ js/vpm_loader.js
@@ -34,22 +34,76 @@ function VpmLoader(base_path, get_view_quaternion, callback) {
 				req.onerror();
 				return;
 			}
-			{//bitrate
-				var now = new Date().getTime();
-				var elapsed = now - m_timestamp;
-				elapsed = Math.max(elapsed, 1);
-				var mbps = 8 * req.response.byteLength / elapsed / 1000;
-				if(m_mbps == 0){
-					m_mbps = mbps;
-				}else{
-					m_mbps = m_mbps*0.9+mbps*0.1;
-				}
-				m_timestamp = now;
-			}
 			m_loaded_framecount++;
 			callback(new Uint8Array(req.response));
 		};
 		req.send(null);
+	}
+	function request_new_frame(){
+		if((m_framecount % m_options.keyframe_interval) == 0) {
+			var q = m_get_view_quaternion();
+			
+			var v = new THREE.Vector3( 0, -1, 0 );
+			v.applyQuaternion( q );
+			v.r = Math.sqrt(v.x*v.x+v.z*v.z);
+			
+			var euler = {
+				x:Math.atan2(v.r, -v.y),
+				y:-Math.atan2(v.x, -v.z),
+			};
+			var x = parseInt(THREE.Math.radToDeg(euler.x));
+			var y = parseInt(THREE.Math.radToDeg(euler.y));
+			var p_angle = 90/m_options.num_per_quarter;
+			var p = Math.round(x/p_angle);
+			var _p = (p <= m_options.num_per_quarter) ? p : m_options.num_per_quarter * 2 - p;
+			var split_y = (_p == 0) ? 1 : 4 * _p;
+			var y_angle = 360/split_y;
+			
+			x = p*p_angle;
+			y = Math.round(y/y_angle)*y_angle;
+			x = (x+360)%360;
+			y = (y+360)%360;
+			if(x == 0 || x == 180) {
+				y = 0;
+			}
+			m_x_deg = x;
+			m_y_deg = y;
+		}
+		
+		++m_framecount;
+		var path = m_base_path + "/" + m_x_deg + "_" + m_y_deg + "/" + m_framecount + ".pif";
+		// console.log(path);
+		loadFile(path, (data) => {
+			var now = new Date().getTime();
+			var elapsed = now - m_timestamp;
+			var wait_ms = Math.max(1000/m_options.fps - elapsed, 1);
+			setTimeout(()=>{
+				var now = new Date().getTime();
+				var elapsed = now - m_timestamp;
+				elapsed = Math.max(elapsed, 1);
+				{//bitrate
+					var mbps = 8 * data.byteLength / elapsed / 1000;
+					if(m_mbps == 0){
+						m_mbps = mbps;
+					}else{
+						m_mbps = m_mbps*0.9+mbps*0.1;
+					}
+					m_timestamp = now;
+				}
+				if(m_frame_callback){
+					m_frame_callback(data);
+				}
+				request_new_frame();
+			}, wait_ms);
+		}, (req) => {
+			if(m_loaded_framecount == 0){
+				console.log("not found : " + req.responseURL);
+			}else{
+				console.log("repeat");
+				m_framecount = 0;
+				request_new_frame();
+			}
+		});
 	}
 	loadFile(base_path+"/config.json", (data)=>{
 		var options = {};
@@ -58,53 +112,8 @@ function VpmLoader(base_path, get_view_quaternion, callback) {
 			options = JSON.parse(txt);
 		}
 		m_options = Object.assign(m_options, options);
+		request_new_frame();
 		m_timer = setInterval(() => {
-			if((m_framecount % m_options.keyframe_interval) == 0) {
-				var q = m_get_view_quaternion();
-				
-				var v = new THREE.Vector3( 0, -1, 0 );
-				v.applyQuaternion( q );
-				v.r = Math.sqrt(v.x*v.x+v.z*v.z);
-				
-				var euler = {
-					x:Math.atan2(v.r, -v.y),
-					y:-Math.atan2(v.x, -v.z),
-				};
-				var x = parseInt(THREE.Math.radToDeg(euler.x));
-				var y = parseInt(THREE.Math.radToDeg(euler.y));
-				var p_angle = 90/m_options.num_per_quarter;
-				var p = Math.round(x/p_angle);
-				var _p = (p <= m_options.num_per_quarter) ? p : m_options.num_per_quarter * 2 - p;
-				var split_y = (_p == 0) ? 1 : 4 * _p;
-				var y_angle = 360/split_y;
-				
-				x = p*p_angle;
-				y = Math.round(y/y_angle)*y_angle;
-				x = (x+360)%360;
-				y = (y+360)%360;
-				if(x == 0 || x == 180) {
-					y = 0;
-				}
-				m_x_deg = x;
-				m_y_deg = y;
-			}
-			
-			++m_framecount;
-			var path = m_base_path + "/" + m_x_deg + "_" + m_y_deg + "/" + m_framecount + ".pif";
-			// console.log(path);
-			loadFile(path, (data) => {
-				if(m_frame_callback){
-					m_frame_callback(data);
-				}
-			}, (req) => {
-				if(m_loaded_framecount == 0){
-					console.log("not found : " + req.responseURL);
-					clearInterval(m_timer);
-				}else{
-					console.log("repeat");
-					m_framecount = 0;
-				}
-			});
 		}, 1000/m_options.fps);
 	})
 	
