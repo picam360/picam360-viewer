61d178ad9a5264d17ae3a3e7aa83984192312219
takumadx <hirayama.takuma@ohmydigifab.com>
2019-11-26 10:19:50 +0900

    keyframe

--- js/vpm_loader.js
+++ js/vpm_loader.js
@@ -14,7 +14,9 @@ function VpmLoader(base_path, get_view_quaternion, callback) {
 	var m_framecount = 0;
 	var m_timer = null;
 	var m_mbps = 0;
-	var m_timestamp = new Date().getTime();;
+	var m_timestamp = new Date().getTime();
+	var m_x_deg = 0;
+	var m_y_deg = 0;
 
 	function loadFile(path, callback, error_callbackk) {
 		var req = new XMLHttpRequest();
@@ -57,33 +59,38 @@ function VpmLoader(base_path, get_view_quaternion, callback) {
 		}
 		m_options = Object.assign(m_options, options);
 		m_timer = setInterval(() => {
-			var q = m_get_view_quaternion();
-			
-			var v = new THREE.Vector3( 0, -1, 0 );
-			v.applyQuaternion( q );
-			v.r = Math.sqrt(v.x*v.x+v.z*v.z);
-			
-			var euler = {
-				x:Math.atan2(v.r, -v.y),
-				y:-Math.atan2(v.x, -v.z),
-			};
-			var x = parseInt(THREE.Math.radToDeg(euler.x));
-			var y = parseInt(THREE.Math.radToDeg(euler.y));
-			var p_angle = 90/m_options.num_per_quarter;
-			var p = Math.round(x/p_angle);
-			var _p = (p <= m_options.num_per_quarter) ? p : m_options.num_per_quarter * 2 - p;
-			var split_y = (_p == 0) ? 1 : 4 * _p;
-			var y_angle = 360/split_y;
-			
-			x = p*p_angle;
-			y = Math.round(y/y_angle)*y_angle;
-			x = (x+360)%360;
-			y = (y+360)%360;
-			if(x == 0 || x == 180) {
-				y = 0;
+			if((m_framecount % m_options.keyframe_interval) == 0) {
+				var q = m_get_view_quaternion();
+				
+				var v = new THREE.Vector3( 0, -1, 0 );
+				v.applyQuaternion( q );
+				v.r = Math.sqrt(v.x*v.x+v.z*v.z);
+				
+				var euler = {
+					x:Math.atan2(v.r, -v.y),
+					y:-Math.atan2(v.x, -v.z),
+				};
+				var x = parseInt(THREE.Math.radToDeg(euler.x));
+				var y = parseInt(THREE.Math.radToDeg(euler.y));
+				var p_angle = 90/m_options.num_per_quarter;
+				var p = Math.round(x/p_angle);
+				var _p = (p <= m_options.num_per_quarter) ? p : m_options.num_per_quarter * 2 - p;
+				var split_y = (_p == 0) ? 1 : 4 * _p;
+				var y_angle = 360/split_y;
+				
+				x = p*p_angle;
+				y = Math.round(y/y_angle)*y_angle;
+				x = (x+360)%360;
+				y = (y+360)%360;
+				if(x == 0 || x == 180) {
+					y = 0;
+				}
+				m_x_deg = x;
+				m_y_deg = y;
 			}
+			
 			++m_framecount;
-			var path = m_base_path + "/" + x + "_" + y + "/" + m_framecount + ".pif";
+			var path = m_base_path + "/" + m_x_deg + "_" + m_y_deg + "/" + m_framecount + ".pif";
 			// console.log(path);
 			loadFile(path, (data) => {
 				if(m_frame_callback){
