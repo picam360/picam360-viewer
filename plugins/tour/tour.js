c2ec5f349511ec32b88fb6216e22da45787a7d54
takumadx <hirayama.takuma@ohmydigifab.com>
2020-01-23 18:21:19 +0900

    tune

--- plugins/tour/tour.js
+++ plugins/tour/tour.js
@@ -97,7 +97,7 @@ var create_plugin = (function() {
 				
 				var dot = dir.dot(view_dir);
 				var active = false;
-				if(dot > 0.9){
+				if(dot > 0.95){
 					active_branch = key;
 					active = true;
 				}
